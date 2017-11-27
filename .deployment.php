fa5bae2ab6c2b5c1940beb000c8476ec97d453b3
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-11-27 15:36:58 +0100

    Added deploy-staging and deploy-production scripts

---
+++ .deployment.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'oeco' => [
+        'remote' => 'ftp://'
+            . getenv('DEPLOY_FTP_USERNAME') . ':' . getenv('DEPLOY_FTP_PASSWORD')
+            . '@' . getenv('DEPLOY_FTP_URL') . '/' . getenv('DEPLOY_SUBDOMAIN'),
+        'local' => '.',
+        'ignore' => file_get_contents('.deployignore'),
+        'allowDelete' => true,
+        'purge' => ['temp/cache'],
+        'preprocess' => false,
+        'after' => [
+            'upload: .env.' . getenv('DEPLOY_ENVIRONMENT') . ' .env'
+        ]
+    ],
+    'tempDir' => __DIR__ . '/temp',
+    'colors' => true,
+];
