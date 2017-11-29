833832e42870699df00bb58f182c1592a10b4205
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-11-29 01:52:30 +0100

    Added News model

---
+++ app/News.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App;
+
+use RuntimeException;
+use InvalidArgumentException;
+use Cocur\Slugify\Slugify;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Oeco news.
+ *
+ * @property string title News title, unique
+ * @property-read string slug Slug, automatically-generated from title
+ * @property string summary Optional summary
+ * @property string position Order to display, or `null` when hidden
+ */
+class News extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'slug', 'title', 'summary', 'position'
+    ];
+
+    /**
+     * Trim and set the title, and update the slug attribute automatically.
+     * @param string $value
+     * @return void
+     */
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = trim($value);
+        $this->attributes['slug'] = self::titleToSlug($value);
+    }
+
+    /**
+     * Prevents setting the slug explicitely
+     * @param string $value
+     * @throw RuntimeException Always throw an exception
+     */
+    public function setSlugAttribute($value)
+    {
+        throw new RuntimeException('Slug cannot be set explicitely, set title instead.');
+    }
+
+    /**
+     * Convert a title to a slug
+     * @param string $title A title
+     * @return string The title, converted to a slug
+     */
+    public static function titleToSlug($title)
+    {
+        $slugify = new Slugify();
+        return $slugify->slugify($title);
+    }
+}
