0e93477c74c0ae1f18887937d5435106dd7346ea
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-11-29 19:44:16 +0100

    Added News table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,12 +5,11 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
-     *
+     * Run the news table.
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(NewsTableSeeder::class);
     }
 }
