7d3ad1adef3d55d14780295ec239de5701cfe525
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-11-29 21:07:51 +0100

    Added json error handling for /api

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
+    use ApiExceptionHandlerTrait;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -48,6 +50,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // Handle API-specific exceptions separately
+        if ($this->isApiCall($request)) {
+            return $this->getJsonResponseForException($request, $exception);
+        }
+
         return parent::render($request, $exception);
     }
 }
