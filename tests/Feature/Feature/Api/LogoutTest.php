45c48ed064509882d614b7cf38571debc380a2f7
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-11-30 16:19:11 +0100

    Added tests for /api/logout and added missing use statement

---
+++ tests/Feature/Feature/Api/LogoutTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Feature\Api;
+
+use Tests\TestCase;
+use App\User;
+
+class LogoutTest extends TestCase
+{
+    public function testUserIsLoggedOutProperly()
+    {
+        $user = factory(User::class)->create(['email' => 'user@test.com']);
+        $token = $user->generateToken();
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $this->json('get', '/api/news', [], $headers)->assertStatus(200);
+        $this->json('post', '/api/logout', [], $headers)->assertStatus(200);
+
+        $user = User::find($user->id);
+        $this->assertEquals(null, $user->api_token);
+    }
+
+    public function testUserWithNullToken()
+    {
+        // Simulating login
+        $user = factory(User::class)->create(['email' => 'user@test.com']);
+        $token = $user->generateToken();
+        $headers = ['Authorization' => "Bearer $token"];
+
+        // Simulating logout
+        $user->api_token = null;
+        $user->save();
+
+        $this->json('get', '/api/news', [], $headers)->assertStatus(401);
+    }
+}
