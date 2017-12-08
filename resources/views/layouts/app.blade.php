335c2d64f4ec3c9d06388fb9cfb364e67dcd07b5
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-12-08 15:26:36 +0100

    Added 'app' layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Metadata -->
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+  </head>
+  <body>
+    @yield('content')
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- LiveReload -->
+    @if(config('app.env') == 'development')
+        <script src="http://localhost:35729/livereload.js"></script>
+    @endif
+  </body>
+</html>
