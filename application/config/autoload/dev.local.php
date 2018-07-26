f27f9fc8497266080077f51bfa3d70b53c70c261
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-07-26 11:28:50 +0200

    Added development database settings

---
+++ application/config/autoload/dev.local.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Development database settings.
+ */
+
+return array(
+  'db' => array(
+    'dsn'      => 'mysql:dbname=oeco_dev;host=127.0.0.1',
+    'username' => 'oeco_dev',
+  ),
+);
