cc40c473cceba01b5172650356b467e4f467b1eb
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-07-26 17:37:33 +0200

    Added Image model

---
+++ application/models/Image.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Models;
+
+/**
+ * Represents an image file
+ */
+class Image
+{
+    /**
+     * Absolute path to the image file.
+     * @var string
+     */
+    public $path;
+
+    /**
+     * Natural width of the image.
+     * @var int
+     */
+    public $width;
+
+    /**
+     * Natural height of the image.
+     * @var int
+     */
+    public $height;
+
+    /**
+     * Create a new Image instance.
+     *
+     * @param string $path Absolute path to the image file.
+     * @param int $width Natural image width.
+     * @param int $height Natural image height.
+     */
+    public function __construct($path, $width, $height)
+    {
+        if (empty($path)) {
+            throw new \InvalidArgumentException('Missing path parameter.');
+        }
+        $this->path = $path;
+        $this->width = $width;
+        $this->height = $height;
+    }
+}
