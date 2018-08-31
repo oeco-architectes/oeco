1c17a8aea0635931f1bd589ec435f588aa938b09
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-31 10:35:17 +0200

    Fixed ESLint linting errors

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,18 +1,5 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-window.Vue = require('vue');
+import Vue from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+Vue.component('example-component', ExampleComponent);
+new Vue({ el: '#app' }); // eslint-disable-line no-new
