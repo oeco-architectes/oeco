a2c1102a989442d1479695f085f65c32611ec3ae
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-08-31 10:13:41 +0200

    Added app.js and app.css to Welcome template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -63,6 +64,7 @@
                 margin-bottom: 30px;
             }
         </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -91,5 +93,11 @@
                 </div>
             </div>
         </div>
+
+        <div id="app">
+            <example-component />
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+        @yield('scripts')
     </body>
 </html>
