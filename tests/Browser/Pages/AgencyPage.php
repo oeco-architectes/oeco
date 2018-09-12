f65328598bf4e3dfdda5342c0d8dfd21c06009c2
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-09-12 20:32:20 +0200

    Added assert() to Dusk base page

--- tests/Browser/Pages/AgencyPage.php
+++ tests/Browser/Pages/AgencyPage.php
@@ -18,17 +18,6 @@ class AgencyPage extends Page
     }
 
     /**
-     * Assert that the browser is on the page.
-     *
-     * @param  Browser  $browser
-     * @return void
-     */
-    public function assert(Browser $browser)
-    {
-        //
-    }
-
-    /**
      * Get the element shortcuts for the page.
      *
      * @return array
--- tests/Browser/Pages/Page.php
+++ tests/Browser/Pages/Page.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser\Pages;
 
+use Laravel\Dusk\Browser;
 use Laravel\Dusk\Page as BasePage;
 
 abstract class Page extends BasePage
@@ -16,5 +17,16 @@ abstract class Page extends BasePage
         return [
             '@element' => '#selector',
         ];
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     *
+     * @param  Browser  $browser
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertPathIs($this->url());
     }
 }
