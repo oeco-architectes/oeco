5548ff726cbfc8d66bf8d48e1008cdb22272ad94
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-09-14 11:20:51 +0200

    Added project styles

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.html5')
 
 @section('content')
-    @foreach ($sections as $section)
-        @if (gettype($section) === 'string')
-            {!! @Markdown::convertToHtml($section) !!}
-        @else
-            <img
-                src="{{ $section->href }}"
-                width="{{ $section->width }}"
-                height="{{ $section->height }}"
-                alt="{{ $section->title }}"
-            />
-        @endif
-    @endforeach
+    <div class="oe-project">
+        @foreach ($sections as $section)
+            @if (gettype($section) === 'string')
+                {!! @Markdown::convertToHtml($section) !!}
+            @else
+                <img
+                    src="{{ $section->href }}"
+                    width="{{ $section->width }}"
+                    height="{{ $section->height }}"
+                    alt="{{ $section->title }}"
+                />
+            @endif
+        @endforeach
+    </div>
 @endsection
