3788aab006b95683907f66769d088217f806ec83
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-09-21 07:44:23 +0200

    Revert "Disabled Chrome sandbox and enabled sudo in Travis CI"
    This reverts commit e0d30a97a8795d97958cbf5c5fe550ec8e91d04f.
    
    This commit caused deployment errors:
    
        Error: Cannot upload file [...], number of retries exceeded. Error: php_connect_nonb() failed: Operation now in progress (115) in vendor/dg/ftp-deployment/src/Deployment/FtpServer.php:118
    
    This error is caused by ports being blocked by firewalls. See https://stackoverflow.com/questions/15688331/php-connect-nonb-failed-operation-now-in-progress-115-happens-intermitten

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -43,6 +43,7 @@ abstract class DuskTestCase extends BaseTestCase
         $options = (new ChromeOptions())->addArguments([
             '--disable-gpu',
             '--headless',
+            '--no-sandbox' // Mandatory for Travis CI, Docker, etc.
         ]);
 
         return RemoteWebDriver::create(
