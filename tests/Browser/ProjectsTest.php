fde7ebd1ace4cb18de1739b576c390f21cf46511
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-10-08 10:27:50 +0200

    Added Browser tests for mozaic tile formats

--- tests/Browser/ProjectsTest.php
+++ tests/Browser/ProjectsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use PHPUnit\Framework\Assert as PHPUnit;
 
 class ProjectsTest extends DuskTestCase
 {
@@ -15,7 +16,18 @@ class ProjectsTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser
                 ->visit(new Pages\ProjectsPage())
-                ->assertElementsCount('@project', 30);
+
+                // Total projects
+                ->assertElementsCount('@project', 30)
+
+                // Each tile format exist
+                ->assertPresent('@project@project-small')
+                ->assertPresent('@project@project-tall')
+                ->assertPresent('@project@project-wide')
+
+                // Each project has only 1 modifier
+                ->assertElementsCount('@project@project-small, @project@project-tall, @project@project-wide', 30)
+                ->assertMissing('@project:not(@project-small):not(@project-tall):not(@project-wide)');
             $this->assertHasNavigationMenu($browser);
         });
     }
