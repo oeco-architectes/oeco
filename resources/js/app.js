ab885dc3d86c99016a736e6533ed131a7021f9bc
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-11-06 10:30:20 +0100

    Replaced ESLint/Stylelint disable-{line -> next-line}

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,5 @@ import Vue from 'vue';
 import VueCarousel from 'vue-carousel';
 
 Vue.use(VueCarousel);
-new Vue({ el: '.oe-container' }); // eslint-disable-line no-new
+// eslint-disable-next-line no-new
+new Vue({ el: '.oe-container' });
